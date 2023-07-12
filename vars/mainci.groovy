0110d0c285759174d08082f4330bdf20e5f6fff9
Raghu K <raghuk.vit@gmail.com>
2023-07-12 08:37:38 +0530

    Update roboshop-jenkins/

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,44 +1,28 @@
 def call() {
-  pipeline {
-
-    agent {
-      node { label 'workstation'}
-    }
-
-    stages {
+  node('workstation') {
 
       if (env.cibuild == "java") {
 
         stage('Build') {
-          steps {
-            sh 'mvn package'
-          }
+          sh 'mvn package'
         }
       }
 
       stage('Unit tests') {
-        steps {
           echo 'unit tests'
-        }
       }
 
       stage('Code Analysis') {
-        steps {
           echo 'sonar'
           //sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.175:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
-        }
       }
 
       stage('Security Scans') {
-        steps {
           echo 'Security Scans'
-        }
       }
 
       stage('Publish a Artifact') {
-        steps {
           echo 'Publish a Artifact'
-        }
       }
 
 
