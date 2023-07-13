a7cd6caeda95ee6e5d9200105392efebe44e69a2
Raghu K <raghuk.vit@gmail.com>
2023-07-13 08:30:18 +0530

    Update roboshop-jenkins/

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,40 +1,58 @@
 def call() {
   node('workstation') {
 
-      stage('Code Checkout') {
-        sh 'env'
-        sh 'find . | grep "^./" |xargs rm -rf'
-        git branch: env.BRANCH_NAME , url: 'https://github.com/raghudevopsb73/frontend'
-      }
+    stage('Code Checkout') {
+      git(
+          url: 'https://github.com/raghudevopsb73/frontend',
+          branch: 'test',
+          changelog: false,
+          poll: false
+      )
 
-      if (env.cibuild == "java") {
-
-        stage('Build') {
-          sh 'mvn package'
-        }
-      }
-
-      stage('Unit tests') {
-          echo 'unit tests'
-        sh 'ls -ltr'
-      }
-
-      stage('Code Analysis') {
-          echo 'sonar'
-          //sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.175:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
-      }
-
-      stage('Security Scans') {
-          echo 'Security Scans'
-      }
-
-      if(env.TAG_NAME ==~ ".*") {
-        stage('Publish a Artifact') {
-          if (env.cibuild == "nginx") {
-            sh 'zip -r ${component}-${TAG_NAME}.zip *'
-          }
-        }
-      }
+//        sh 'env'
+//        sh 'find . | grep "^./" |xargs rm -rf'
+//        if(env.TAG_NAME ==~ ".*") {
+//          git(
+//              url: 'https://github.com/raghudevopsb73/frontend',
+//              branch: 'v1',
+//              changelog: false,
+//              poll: false
+//          )
+//        } else {
+//          //git branch: env.BRANCH_NAME , url: 'https://github.com/raghudevopsb73/frontend'
+//          echo 'hello'
+//        }
 
     }
+
+    if (env.cibuild == "java") {
+
+      stage('Build') {
+        sh 'mvn package'
+      }
+    }
+
+    stage('Unit tests') {
+      echo 'unit tests'
+      sh 'ls -ltr'
+    }
+
+    stage('Code Analysis') {
+      echo 'sonar'
+      //sh 'sonar-scanner -Dsonar.host.url=http://172.31.83.175:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=frontend -Dsonar.qualitygate.wait=true'
+    }
+
+    stage('Security Scans') {
+      echo 'Security Scans'
+    }
+
+    if(env.TAG_NAME ==~ ".*") {
+      stage('Publish a Artifact') {
+        if (env.cibuild == "nginx") {
+          sh 'zip -r ${component}-${TAG_NAME}.zip *'
+        }
+      }
+    }
+
+  }
 }
