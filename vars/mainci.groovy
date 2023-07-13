69a694b3b55f8b030d1ac376bf8e03e3ffb35697
Raghu K <raghuk.vit@gmail.com>
2023-07-13 08:30:49 +0530

    Update roboshop-jenkins/

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -4,7 +4,7 @@ def call() {
     stage('Code Checkout') {
       git(
           url: 'https://github.com/raghudevopsb73/frontend',
-          branch: 'test',
+          branch: 'v1',
           changelog: false,
           poll: false
       )
